99ebccb6c84ca649ce8f64ad367c963c6de500c6
tarunpodavu1 <tarinkumarpodavu4@gmail.com>
2024-03-03 20:49:25 +0530

    Fetching Movie from MongoDb done

---
+++ src/main/java/com/dev/tarun/movies/Review.java
@@ -0,0 +1,42 @@
+package com.dev.tarun.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection="reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+
+	@Id
+	private ObjectId id;
+	private String body;
+	public ObjectId getId() {
+		return id;
+	}
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public Review(ObjectId id, String body) {
+		super();
+		this.id = id;
+		this.body = body;
+	}
+	public Review() {
+		super();
+	}
+	
+	
+}
